a87af8ff21386e72e7766bb08344fc2b725b984d
tonio155 <tonio155@gmail.com>
2017-08-05 11:09:44 +0100

    indentation fixed

--- schema.js
+++ schema.js
@@ -2,47 +2,47 @@ const axios = require('axios');
 
 const {
     // import from GraphQL all the object types that we will work with
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLSchema,
-    GraphQLList,
-    GraphQLNonNull
+  GraphQLObjectType,
+  GraphQLString,
+	GraphQLInt,
+	GraphQLSchema,
+	GraphQLList,
+	GraphQLNonNull
 } = require('graphql');
 
 
 const CustomerType = new GraphQLObjectType({
-    name: 'Customer',
-    fields: () => ({
-        id: {type: GraphQLString},
-        name: {type: GraphQLString},
-        email: {type: GraphQLString},
-        age: {type: GraphQLInt},
-    })
+	name: 'Customer',
+	fields: () => ({
+		id: {type: GraphQLString},
+		name: {type: GraphQLString},
+		email: {type: GraphQLString},
+		age: {type: GraphQLInt},
+	})
 });
 
 // Root query (always required)
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        customer: {
-            type: CustomerType,
-            args: {
-                id:{ type: GraphQLString },
-            },
-            resolve(parentValue, args){
-								return axios.get('http://localhost:3000/customers/' + args.id)
-									.then(res => res.data);
-            }
-				},
-				customers: {
-					type: new GraphQLList(CustomerType),
-					resolve(parentValue, args){
-						return axios.get('http://localhost:3000/customers/')
-									.then(res => res.data);
-					}
-				}
-    }
+	name: 'RootQueryType',
+	fields: {
+		customer: {
+			type: CustomerType,
+			args: {
+				id:{ type: GraphQLString },
+			},
+			resolve(parentValue, args){
+				return axios.get('http://localhost:3000/customers/' + args.id)
+					.then(res => res.data);
+			}
+		},
+		customers: {
+			type: new GraphQLList(CustomerType),
+			resolve(parentValue, args){
+				return axios.get('http://localhost:3000/customers/')
+					.then(res => res.data);
+			}
+		}
+	}
 });
 
 // Mutations
@@ -95,6 +95,6 @@ const mutation = new GraphQLObjectType({
 });
 
 module.exports = new GraphQLSchema({
-		query: RootQuery,
-		mutation
+	query: RootQuery,
+	mutation
 });
